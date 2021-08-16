b3e7e52a48cf4a251eca4be17322b94ddefeba02
PictureElement <marios.sofokleous@yandex.com>
2021-08-16 21:49:38 +0300

    Implement support for October CMS v2.0. Replace ArrizalAmin.Portfolio dependency with Msof.Portfolio. Various UI/UX improvements.

--- README.md
+++ README.md
@@ -8,20 +8,23 @@ A polished single-page business-oriented theme with blog for October CMS.
 ## Features/Highlights
 
 * Free and open-source
-* Blog (Blog page, Category page, Tag page, Post page, Search function)
+* Blog (Categories, Tags, Popular posts, Related posts, Search function, Social share buttons)
 * Easy theming
-* Optimized for Speed
-* CSS Animations
+* Optimized for speed
+* CSS animations
 * High quality
 * SEO friendly
 * Responsive Web Design
-* BEM naming
-* System fonts/Google Fonts
-* Contact and Subscription forms
-* 404, 500 and Maintenance pages
+* BEM naming (Sass files included)
+* System/Google fonts selection
+* Masonry portfolio with category filters
+* GDPR compliance
+* Open Graph support
+* Contact & Subscription forms
+* 404, 500 & Maintenance pages
 
 ## License
 
 You can fork this repo is completely free. Code released under the [MIT](LICENSE) license.
 
-Developed by [Marios Sofokleous](https://msof.me/).
+Developed by [Marios Sofokleous](https://www.msof.me/).
--- assets/js/rapido.js
+++ assets/js/rapido.js
@@ -1,1 +1,229 @@
-$(document).ready(function(){var e,s,a;$(".grid__controls button.btn").click(function(){$(".grid__controls").find(".active").removeClass("active"),$(this).addClass("active")}),$("#pageHeader").length&&(e=$("#pageHeader"),0<$(window).scrollTop()&&e.addClass("header_shadow"),$(".navbar-toggler").on("click",function(){var a=$(".navbar-collapse").hasClass("show"),o=0<$(window).scrollTop();e.hasClass("page-header_shadow");!o&&a?e.removeClass("page-header_shadow"):e.addClass("page-header_shadow")}),$(window).scroll(function(){var a=$(".navbar-collapse").hasClass("show"),o=0<$(window).scrollTop();e.hasClass("page-header_shadow");o||a?e.addClass("page-header_shadow"):e.removeClass("page-header_shadow")})),$("#toTop").length&&$("#toTop").on("click",function(a){event.preventDefault(),$("html, body").animate({scrollTop:0},1e3,function(){})}),window.counterUp&&(s=window.counterUp.default,$(".counter").each(function(a,o){new Waypoint({element:$(this),handler:function(){s(o,{duration:2e3,delay:16}),this.destroy()},offset:"bottom-in-view"})})),$("#heroCarousel").length&&$("#heroCarousel").owlCarousel({loop:!0,pullDrag:!1,mouseDrag:!1,margin:0,autoplay:!1,checkVisibility:!1,items:1,navText:['<i class="fas fa-long-arrow-alt-left"></i>','<i class="fas fa-long-arrow-alt-right"></i>'],responsive:{0:{nav:!1,dots:!0},992:{nav:!0,dots:!1}}}),$("#clientsCarousel").length&&$("#clientsCarousel").owlCarousel({loop:!0,nav:!1,dots:!1,autoplay:!0,autoplayTimeout:2e3,autoplayHoverPause:!0,margin:16,responsive:{0:{items:3},768:{items:4},992:{items:6}}}),$("#testimonialsCarousel").length&&$("#testimonialsCarousel").owlCarousel({loop:!0,margin:0,nav:!1,dots:!0,autoplay:!1,responsive:{0:{items:1}}}),$("#blogCarousel").length&&$("#blogCarousel").owlCarousel({loop:!1,nav:!1,dots:!1,autoplay:!1,responsive:{0:{items:1,stagePadding:24,margin:12},992:{items:2,stagePadding:32,margin:16}}}),$("#singlePostCarousel").length&&$("#singlePostCarousel").owlCarousel({loop:!0,nav:!0,navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],dots:!1,autoplay:!1,items:1}),$("[data-fancybox]").length&&$("[data-fancybox]").fancybox({smallBtn:!0,hideScrollbar:!0}),document.body.contains(document.getElementById("pageHeader"))&&(a=document.getElementById("pageHeader"),new Headroom(a,{offset:500}).init()),$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(a){var o;location.pathname.replace(/^\//,"")!=this.pathname.replace(/^\//,"")||location.hostname!=this.hostname||(o=(o=$(this.hash)).length?o:$("[name="+this.hash.slice(1)+"]")).length&&(a.preventDefault(),$("html, body").animate({scrollTop:o.offset().top},1e3,function(){}))}),AOS.init(),window.console.log.apply(console,["%c Made by Marios Sofokleous | msof.me %c %c🤘 %c\n","color: #fff; background: #0020f4; padding:5px 0;","color: #fff; background: #242424; padding:5px 0 5px 5px;","background: #242424; padding:5px 0","background: #242424; padding:5px 5px 5px 0"])}),$(window).on("load",function(){$("#spinnerWrapper").fadeOut("slow"),$("html").css("overflow-y","visible")});
+$(document).ready(function() {
+    // 1. Back to top
+    if ($('#toTop').length) {
+        $('#toTop').on('click', function(e) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 1000, function () {
+                // Callback after animation
+            });
+        });
+    }
+
+    // 2. Counters
+    if (window.counterUp) {
+        var counterUp = window.counterUp["default"]; // Import counterUp2
+        var $el = $('.counter');
+
+        $el.each(function (ignore, counter) {
+            var waypoint = new Waypoint({
+                element: $(this),
+                handler: function() { 
+                    counterUp(counter, {
+                        duration: 2000,
+                        delay: 16
+                    });
+                    this.destroy();
+                },
+                offset: 'bottom-in-view',
+            });
+        });
+    }
+
+    // 3. Hero carousel
+    if ($('#heroCarousel').length) {
+        $('#heroCarousel').owlCarousel({
+            loop:true,
+            pullDrag: false,
+            mouseDrag: false,
+            margin:0,
+            autoplay:false,
+            checkVisibility: false,
+            items: 1,
+            navText: ['<i class="fas fa-long-arrow-alt-left"></i>', '<i class="fas fa-long-arrow-alt-right"></i>'],
+            responsive : {
+                0: {
+                    nav:false,
+                    dots:true
+                },
+                992 : {
+                    nav:true,
+                    dots:false
+                }
+            }
+        });
+    }
+    
+    // 4. Clients carousel
+    if ($('#clientsCarousel').length) {
+        $('#clientsCarousel').owlCarousel({
+            loop:true,
+            nav:false,
+            dots: false,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            margin:16,
+            responsive:{
+                // breakpoint from 0 up
+                0:{
+                    items:3
+                },
+                // breakpoint from 768px up
+                768 : {
+                    items:4
+                },
+                // breakpoint from 992px up
+                992 : {
+                    items:6
+                }
+            }
+        });
+    }
+
+    // 5. Testimonials carousel
+    if ($('#testimonialsCarousel').length) {
+        $('#testimonialsCarousel').owlCarousel({
+            loop:true,
+            margin:0,
+            nav:false,
+            dots: true,
+            autoplay:false,
+            responsive:{
+                // breakpoint from 0 up
+                0:{
+                    items:1
+                }
+            }
+        });
+    }
+
+    // 6. Blog carousel
+    if ($('#blogCarousel').length) {
+        $('#blogCarousel').owlCarousel({
+            loop:false,
+            nav:false,
+            dots: false,
+            autoplay:false,
+            responsive:{
+                // breakpoint from 0 up
+                0:{
+                    items:1,
+                    stagePadding: 24,
+                    margin:12
+                },
+                // breakpoint from 992px up
+                992 : {
+                    items:2,
+                    stagePadding: 32,
+                    margin:16
+                }
+            }
+        });
+    }
+
+    // 7. Post carousel
+    if ($('#singlePostCarousel').length) {
+        $('#singlePostCarousel').owlCarousel({
+            loop:true,
+            nav:true,
+            navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+            dots: false,
+            autoplay:false,
+            items: 1
+        });
+    }
+
+    // 8. Fancybox options
+    if ($('[data-fancybox]').length) {
+        $('[data-fancybox]').fancybox({
+            smallBtn : true,
+            hideScrollbar: true
+        });
+    }
+
+    // 9. Smooth scrolling
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="javascript:;"]')
+        .not('[data-fancybox]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                    });
+                }
+            }
+        });
+
+    // 10. Initialize AOS
+    AOS.init();
+    
+    // 11. Copy to clipboard
+    if ($('#copyLink').length) {
+        $('#copyLink').tooltip({
+            trigger: 'click',
+            placement: 'bottom'
+        });
+
+        function setTooltip(message) {
+            $('#copyLink').tooltip('hide').attr('data-original-title', message).tooltip('show');
+        }
+
+        function hideTooltip(message) {
+            setTimeout(function() {
+                $('#copyLink').tooltip('hide');
+            }, 1000);
+        }
+
+        var clipboard = new ClipboardJS('#copyLink');
+
+        clipboard.on("success", function(e) {
+            setTooltip('Copied!');
+            hideTooltip();
+        });
+
+        clipboard.on("error", function(e) {
+            setTooltip('Failed!');
+            hideTooltip();
+        });
+    }
+
+    // 12. Detect when jump links section get pinned
+    if ($('#jumpLinks').length) {
+        const el = document.querySelector("#jumpLinks")
+        const observer = new IntersectionObserver( 
+            ([e]) => e.target.classList.toggle("jump-links_pinned", e.intersectionRatio < 1),
+            { threshold: [1] }
+        );
+        observer.observe(el);
+    }
+
+    // 13. Copyright
+    window.console.log.apply(console, ["%c Made by Marios Sofokleous | msof.me %c %c🤘 %c\n", "color: #fff; background: #114F7F; padding:5px 0;", "color: #fff; background: #000; padding:5px 0 5px 5px;", "background: #000; padding:5px 0", "background: #000; padding:5px 5px 5px 0"]);
+});
+
+$(window).on('load', function() {
+    // 1. Hide spinner on page load
+    $('#spinnerWrapper').fadeOut('slow');
+    $('html').css('overflow-y', 'visible');
+    
+    // 2. Refresh AOS
+    AOS.refresh();
+});
